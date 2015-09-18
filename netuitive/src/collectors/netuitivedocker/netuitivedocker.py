6ab809edea58c89ef6cc6f9f1f4ad519ee1c5e3b
Shawn Butts <me@shawnbutts.com>
2015-09-18 10:14:48 -0400

    docker plugin update

--- netuitive/src/collectors/netuitivedocker/netuitivedocker.py
+++ netuitive/src/collectors/netuitivedocker/netuitivedocker.py
@@ -46,11 +46,13 @@ class NetuitiveDockerCollector(diamond.collector.Collector):
         def print_metric(cc, name):
             data = cc.stats(name)
             metrics = json.loads(data.next())
+            if name.find("/") != -1:
+                name = name.rsplit('/', 1)[1]
             # memory metrics
             self.memory = self.flatten_dict(metrics['memory_stats'])
             for key, value in self.memory.items():
                 metric_name = name + ".memory." + key
-                self.publish_counter(metric_name, value)
+                self.publish_gauge(metric_name, value)
             # cpu metrics
             self.cpu = self.flatten_dict(metrics['cpu_stats'])
             for key, value in self.cpu.items():
