a67435fdce07934df597aace046448be521ea390
shawnbutts <me@shawnbutts.com>
2016-04-12 15:18:36 -0400

    clean up build.sh

--- build.sh
+++ build.sh
@@ -15,14 +15,6 @@ if [ "$1" == "deb" ] ||  [ "$1" == "all" ]; then
     echo "Running container..."
     docker run -i --rm=true --name agent-builder-deb -v `pwd`:/vagrant agent-builder-deb
 
-    echo "Removing container..."
-    docker stop agent-builder-deb
-    sleep 1
-    docker rm agent-builder-deb
-    sleep 1
-    echo "Removing image..."
-    docker rmi agent-builder-deb
-
 fi
 
 if [ "$1" == "rpm" ] ||  [ "$1" == "all" ]; then
@@ -33,13 +25,5 @@ if [ "$1" == "rpm" ] ||  [ "$1" == "all" ]; then
 
     echo "Running container..."
     docker run -i --rm=true --name agent-builder-rpm -v `pwd`:/vagrant agent-builder-rpm
-
-    echo "Removing container..."
-    docker stop agent-builder-rpm
-    sleep 1
-    docker rm agent-builder-rpm
-    sleep 1
-    echo "Removing image..."
-    docker rmi agent-builder-rpm
 
 fi
