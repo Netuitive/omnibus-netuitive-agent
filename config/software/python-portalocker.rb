3a8b5776c454868d8093c49e8389f47915cc6c6a
Keith Gable <keith@art19.com>
2020-10-02 19:15:27 -0700

    Have to use -U because it's already installed

--- config/software/python-portalocker.rb
+++ config/software/python-portalocker.rb
@@ -5,5 +5,6 @@ dependency "python"
 dependency "pip"
 
 build do
-  command "#{install_dir}/embedded/bin/pip install -I -t #{install_dir}/embedded/lib/python2.7 #{name}==#{version}"
+  # this is -U because portalocker is already installed and we need to downgrade it
+  command "#{install_dir}/embedded/bin/pip install -U -t #{install_dir}/embedded/lib/python2.7 #{name}==#{version}"
 end
